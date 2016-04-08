861e51b60e59a7e468ccc2a01f57765272f5406a
ZhangQiang <zhangqiangoffice@163.com>
2016-04-08 15:49:00 +0800

    完成选项菜单设置

--- src/com/bignerdranch/android/criminalintent/CrimeListFragment.java
+++ src/com/bignerdranch/android/criminalintent/CrimeListFragment.java
@@ -2,9 +2,12 @@ package com.bignerdranch.android.criminalintent;
 
 import java.util.ArrayList;
 
+import android.annotation.TargetApi;
 import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
 import android.support.v4.app.ListFragment;
+import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
@@ -18,6 +21,7 @@ import android.widget.TextView;
 public class CrimeListFragment extends ListFragment {
 	
 	private ArrayList<Crime> mCrimes;
+	private boolean mSubtitleVisible;
 	
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
@@ -29,6 +33,9 @@ public class CrimeListFragment extends ListFragment {
 		CrimeAdapter adapter = new CrimeAdapter(mCrimes);
 		
 		setListAdapter(adapter);
+		
+		setRetainInstance(true);
+		mSubtitleVisible = false;
 	}
 
 	@Override
@@ -76,8 +83,13 @@ public class CrimeListFragment extends ListFragment {
 	public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
 		super.onCreateOptionsMenu(menu, inflater);
 		inflater.inflate(R.menu.fragment_crime_list, menu);
+		MenuItem showSubtitle = menu.findItem(R.id.menu_item_show_subtitle);
+		if (mSubtitleVisible && showSubtitle != null) {
+			showSubtitle.setTitle(R.string.hide_subtitle);
+		}
 	}
 
+	@TargetApi(11)
 	@Override
 	public boolean onOptionsItemSelected(MenuItem item) {
 		switch (item.getItemId()) {
@@ -88,9 +100,35 @@ public class CrimeListFragment extends ListFragment {
 			i.putExtra(CrimeFragment.EXTRA_CRIME_ID, crime.getId());
 			startActivityForResult(i, 0);
 			return true;
+		case R.id.menu_item_show_subtitle:
+			if (getActivity().getActionBar().getSubtitle() == null) {
+				getActivity().getActionBar().setSubtitle(R.string.subtitle);
+				mSubtitleVisible = true;
+				item.setTitle(R.string.hide_subtitle);
+			} else {
+				getActivity().getActionBar().setSubtitle(null);
+				mSubtitleVisible = false;
+				item.setTitle(R.string.show_subtitle);
+			}
+			return true;
 		default:
 			return super.onOptionsItemSelected(item);
 		}
 	}
+
+	@TargetApi(11)
+	@Override
+	public View onCreateView(LayoutInflater inflater, ViewGroup container,
+			Bundle savedInstanceState) {
+		if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.HONEYCOMB) {
+			if (mSubtitleVisible) {
+				getActivity().getActionBar().setSubtitle(R.string.subtitle);
+			}
+		}
+		return super.onCreateView(inflater, container, savedInstanceState);
+	}
+	
+	
+	
 	
 }
