c58886a2c66d2c2e3799159aa05c6c30e2037fe3
zhangqiangoffice <zhangqiangoffice@163.com>
2016-04-09 23:15:28 +0800

    完成ImageFragment

--- src/com/bignerdranch/android/criminalintent/CrimeFragment.java
+++ src/com/bignerdranch/android/criminalintent/CrimeFragment.java
@@ -34,6 +34,7 @@ public class CrimeFragment extends Fragment {
 	public static final String EXTRA_CRIME_ID =
 			"com.bignerdranch.android.criminalintent.crime_id";
 	private static final String DIALOG_DATE = "date";
+	private static final String DIALOG_IMAGE = "image";
 	private static final int REQUEST_DATE = 0;
 	private static final int REQUEST_PHOTO = 1;
 	
@@ -137,6 +138,20 @@ public class CrimeFragment extends Fragment {
 		}
 		
 		mPhotoView = (ImageView) v.findViewById(R.id.crime_imageView);
+		mPhotoView.setOnClickListener(new View.OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				Photo p = mCrime.getPhoto();
+				if (p == null) {
+					return;
+				}
+				FragmentManager fm = getActivity().getSupportFragmentManager();
+				String path = getActivity().getFileStreamPath(p.getFilename()).getAbsolutePath();
+				ImageFragment.newInstance(path).show(fm, DIALOG_IMAGE);
+				
+			}
+		});
 		
 		return v;
 	}
